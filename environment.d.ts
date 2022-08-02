43dc6f2c85e31c44cd1db350a354f8cf34e3eaa9
Belazor <belazor@icloud.com>
2022-08-02 11:09:33 +0100

    - Update ENV helper file

--- environment.d.ts
+++ environment.d.ts
@@ -1,21 +1,19 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
+			NEXT_PUBLIC_APP_NAME: string;
+
 			APP_ENV: string;
 			APP_DEBUG: boolean;
 
 			COOKIE_NAME: string;
 			COOKIE_SALT: string;
 
-			DB_CONNECTION: string;
 			DB_HOST: string;
 			DB_PORT: number;
 			DB_DATABASE: string;
 			DB_USERNAME: string;
 			DB_PASSWORD: string;
-
-			DISCORD_CLIENT_ID: string;
-			DISCORD_CLIENT_SECRET: string;
 
 			NODE_ENV: 'development' | 'production';
 			PORT?: string;
