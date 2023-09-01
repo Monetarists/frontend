d5deba11dbbab0047651516403ce7690768883f7
Fillip H. <9424286+filliph@users.noreply.github.com>
2023-09-01 23:58:09 +0100

    Fix flashing issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,13 @@ import "../tools/whyDidYouRender";
 
 import "../styles/global.scss";
 
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import type { AppContext, AppProps } from "next/app";
+import {
+	ChakraProvider,
+	ColorModeProvider,
+	cookieStorageManagerSSR,
+	localStorageManager,
+} from "@chakra-ui/react";
 import { getCookie } from "cookies-next";
 import { i18n } from "@lingui/core";
 import { I18nProvider } from "@lingui/react";
@@ -14,6 +19,12 @@ import { messages as messagesFr } from "../locales/fr/messages";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { Language } from "../@types/Language";
+import theme from "../layouts/theme";
+import App from "next/app";
+
+interface MonetaristAppProps extends AppProps {
+	cookies?: string;
+}
 
 function parseLang(lang: string): Language {
 	if (lang === "ja") {
@@ -34,18 +45,28 @@ i18n.load({
 	fr: messagesFr,
 });
 
-function Monetarist({ Component, pageProps }: AppProps) {
+function Monetarist({ Component, pageProps, cookies }: MonetaristAppProps) {
 	const router = useRouter();
 	const lang = parseLang(getCookie("monetarist_language") ?? "en");
 
 	i18n.activate(lang);
+
+	const colorModeManager =
+		typeof cookies === "string"
+			? cookieStorageManagerSSR(cookies)
+			: localStorageManager;
 
 	return (
 		<>
 			<Head>
 				<title key="title">{process.env.NEXT_PUBLIC_APP_NAME}</title>
 			</Head>
-			<ChakraProvider>
+			<ChakraProvider
+				resetCSS
+				theme={theme}
+				colorModeManager={colorModeManager}
+			>
+				<ColorModeProvider options={theme.config} />
 				<I18nProvider i18n={i18n}>
 					<Component {...pageProps} key={router.asPath} />
 				</I18nProvider>
@@ -53,6 +74,11 @@ function Monetarist({ Component, pageProps }: AppProps) {
 		</>
 	);
 }
+
+Monetarist.getInitialProps = async (appCtx: AppContext) => {
+	const appProps = await App.getInitialProps(appCtx);
+	return { ...appProps, cookies: appCtx.ctx.req?.headers?.cookie };
+};
 
 Monetarist.whyDidYouRender = true;
 
